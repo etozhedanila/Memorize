464b88b820e31aa549a6b70108411efcfd20df16
Субботин Виталий Александрович <vitaly_subbotin@mail.ru>
2021-05-07 13:16:09 +0300

    first lesson

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Виталий Субботин on 07.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct CardView: View {
+    
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 3)
+                Text("❤️")
+            } else {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+        }
+    }
+}
